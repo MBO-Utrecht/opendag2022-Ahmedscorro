af8cebbaeba97db63e4cb4232c5eb666e7fcd69b
ahmedhe_ <ahmedhedni@hotmail.nl>
2022-06-28 23:00:32 +0200

    Toevoeging van Overige Informatie pagina en javascript bestand.

---
+++ overige-info.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!-- Meta tags -->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="all">
+    <meta name="language" content="Dutch">
+    <meta name="author" content="Ahmed al Hedni">
+    <meta name="description" content="">
+    <meta name="keywords" content="OPENDAG, ICT">
+    <meta name="copyright" content="copyright">
+
+    <!-- CSS link -->
+    <link rel="stylesheet" href="style.css">
+
+    <!-- Hamburger link for small screens -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Website title -->
+    <title>MBO Utrecht</title>
+</head>
+
+<body>
+    <div class="topnav">
+        <img src="https://www.mboutrecht.nl/wp-content/themes/mboutrecht/img/output/logo-new@2x.png" class="responsive">
+        <div class="topnav" id="myTopnav">
+            <ul>
+                <a href="./index.php">Home</a>
+                <a href="./overige-info.php">Overige Informatie</a>
+                <a href="./enquete.php">Enquête </a>
+                <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                    <i class="fa fa-bars"></i>
+            </ul>
+            </a>
+        </div>
+    </div>
+    <main>
+        <div id="about">
+            <div class="div">
+                <h1>Op deze pagina kun je alle informatie vinden over de opleidingen die je kan volgen op onze locatie!</h1>
+                <br>
+                <p><b>Klik gauw op de knop hieronder om meer informatie<br>
+                        over de opendagen te krijgen!</b></p>
+                <br>
+                <button type="button" class="button1"><a href="#section"><span></span>OPEN DAGEN</a></button>
+            </div>
+
+
+        </div>
+        <div class="section" id="section">
+            <div class="container">
+                <div class="content-section">
+                    <div class="title">
+                        <h1>Overige Informatie</h1>
+                    </div>
+                    <div class="content">
+                        <h3>Wij hebben op deze locatie meerdere opleidingen die je kan volgen.
+                            <br> Hieronder kan je alvast zien op welke dagen en tijdstippen wij onze open dagen hebben voor de opleidingen.
+                        </h3>
+                        <p>Op onze locatie kun je de volgende opleidingen volgen:
+                            <br><strong>Software Development, Leidinggevende travel & hospitality en Marketing.</strong>
+                        </p>
+                        <p>Voor meer informatie over deze opleidingen kun je klikken op de button hieronder, of je scrollt naar beneden.</p>
+                        <p>Tevens kan je ook misschien al een ideetje krijgen van de binnenkant van het gebouw door de plattegrond hiernaast te bekijken!</p>
+                        <div class="button">
+                            <a href="#opleidingen">Opleidingen</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="image-section">
+                    <img src="./img/plattegrond.PNG" alt="">
+                </div>
+            </div>
+            <div class="icons">
+                <div class="prijzengeld">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="55" height="50" fill="currentColor" class="bi bi-calendar" viewBox="0 0 16 16">
+                        <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
+                    </svg>
+                    <h2>15 november 2022</h2>
+                    <h4><b>18:00 tot 20:30</b></h4>
+                </div>
+                <div class="trophies">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="55" height="50" fill="currentColor" class="bi bi-calendar" viewBox="0 0 16 16">
+                        <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
+                    </svg>
+                    <h2>21 januari 2023</h2>
+                    <h4><b>10:00 tot 13:00</b></h4>
+                </div>
+                <div class="calendar">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="55" height="50" fill="currentColor" class="bi bi-calendar" viewBox="0 0 16 16">
+                        <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
+                    </svg>
+                    <h2>16 maart 2023</h2>
+                    <h4><b>18:00 tot 20:30</b></h4>
+                </div>
+            </div>
+        </div>
+
+        <section id="opleidingen">
+            <h1 id="titel2">Opleidingen</h1>
+            <h3 class="text1">
+                Hieronder staan de drie opleidingen die je kunt volgen.
+            </h3>
+            <div class="container2">
+                <div class="column1">
+                    <a href="#form"><img src="./img/sd.jpg" style="width: auto; height: 150px;"></a>
+                    <h3>Software Development (Niveau 4)</h3>
+                    <p class="e_1">De opleiding <strong>Software Development</strong> is een veel gekozen opleiding, lees hieronder wat de opleiding precies inhoudt. </p>
+                    <br>
+                    <p>Tijdens deze drie tot vier-jarige opleiding leer je hoe je zelfstandig apps, games of webapplicaties kunt maken met behulp van frameworks en object georiënteerd programmeren.
+                        <br>
+                        Het is een echte programmeeropleiding, geen knippen en plakken maar degelijk codekloppen.
+                        <br>
+                        Je verdiept je in nieuwe ontwikkelingen zoals AI en Big Data. Je werkt projectmatig en gebruikt programmeertalen zoals PHP, C#, Typescript en SQL.
+                        <br>
+                        Je ontwikkelt je beroepshouding, presentatietechnieken en leert samen te werken. Je leert een product ontwikkelen, bouwen, testen, documenteren en opleveren.
+                        <br>
+                        Naarmate je verder komt in je studie ga je je niet alleen verder verdiepen/verbreden door middel van keuzedelen maar leer je ook je werk te documenteren.
+                        <br>
+                        De opleiding sluit je in het derde / vierde leerjaar af met een beroepsgericht examen.
+                        <br><br>
+                        Geldt één van de onderstaande situaties voor jou? Dan mag je je aanmelden voor deze opleiding:
+                        <br>
+                        - Je hebt een diploma vmbo kader, GL, TL of MAVO
+                        <br>
+                        - Je hebt een overgangsbewijs van havo 3 naar havo 4
+                        <br>
+                        - Diploma mbo niveau 2 of 3
+                    </p>
+                </div>
+                <div class="column2">
+                    <a href="#form"><img src="./img/ld.jpg" style="width: auto; height: 150px;"></a>
+                    <h3>Leidinggevende travel & hospitality (Niveau 4)</h3>
+                    <p class="e_2">Vind je het leuk om in de toeristische sector te werken, reis je graag én wil je een leidinggevende functie?
+                        <br> Dan is de opleiding <strong>Leidinggevende travel & hospitality</strong> bij MBO Utrecht misschien wel iets voor jou!
+                    </p> <br>
+                    <p>Tijdens de opleiding leren wij jou hoe je frontoffice werkzaamheden uitvoert en gasten ontvangt in een toeristische omgeving, maar ook hoe je het beleid van het bedrijf vertaalt naar de werkvloer,
+                        <br>
+                        een team aanstuurt en hoe je voorraden beheert.
+                        <br>
+                        Wat je leert kun je direct in de praktijk brengen tijdens de verschillende projecten op school.
+                        <br>
+                        Tijdens de praktijkuren die je o.a. maakt voor ons eigen leerbedrijf EventLab op evenementen als Summer Lake en Halloween Fright Night leer je de beginselen van hospitality.
+                        <br>
+                        Je taken bestaan uit de verkoop van producten en diensten aan klanten/gasten. Ook verstrek je toeristische informatie in de breedste zin van het woord.
+                        <br>
+                        Je bent verantwoordelijk voor de uitvoering van het beleid van het bedrijf en de vertaling hiervan naar jouw afdeling.
+                        <br><br>
+                        Geldt één van de onderstaande situaties voor jou? Dan mag je je aanmelden voor deze opleiding:
+                        <br>
+                        - Je hebt een diploma vmbo kader, GL, TL of MAVO
+                        <br>
+                        - Je hebt een overgangsbewijs van havo 3 naar havo 4
+                        <br>
+                        - Diploma mbo niveau 2 of 3
+                        <br>
+                    </p>
+                </div>
+                <div class="column3">
+                    <a href="#form"><img src="./img/mk.jpg" style="width: auto; height: 150px;"></a>
+                    <h3>Marketing en commerciële economie (Niveau 4)</h3>
+                    <p class="e_3">De commerciële wereld is een snelle wereld. Geen tijd om achterover te leunen! Ben jij creatief en innovatief en wil je leren hoe je slimme marketingcampagnes maakt om een product in de markt te zetten?
+                        Dan is de opleiding <strong>Marketing en commerciële economie</strong> echt iets voor jou.
+                    </p>
+                    <p>De commerciële wereld is een snelle wereld die voortdurend in verandering is. Geen tijd om achterover te leunen!
+                        <br>
+                        Wij leren je doelgroepen in kaart te brengen en trends te onderzoeken. Marketingplannen en -beleid te maken en te beoordelen.
+                        <br>
+                        Met welke reclamestrategie je een product het best in de markt zet, hoe je social media inzet om jouw doelgroep snel te bereiken en hoe je (financiële) budgetten bewaakt.
+                        <br>
+                        Ook leren we je om goed samen te werken en innovatief te zijn. Zo zul je tijdens de opleiding bijvoorbeeld zelf een product bedenken, ontwikkelen en in de markt zetten.
+                        <br>
+                        Deze opleiding is voor ambitieuze studenten met interesse voor economie. Naast de reguliere vakken op mbo niveau 4 krijg je met de keuzedelen ook al lesstof uit het propedeusejaar van het hbo.
+                        <br><br>
+                        Geldt één van de onderstaande situaties voor jou? Dan mag je je aanmelden voor deze opleiding:
+                        <br>
+                        - Je hebt een diploma vmbo kader, GL, TL of MAVO
+                        <br>
+                        - Je hebt een overgangsbewijs van havo 3 naar havo 4
+                        <br>
+                        - Diploma mbo niveau 2 of 3
+                        <br>
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <footer class="sticky-footer">
+            <!-- <h2>Footer Stick to the Bottom</h2> -->
+            <ul>
+                <li><a href="./index.php">Home</a></li>
+                <li><a href="./overige-info.php">Overige Informatie</a></li>
+                <li><a href="./enquete.php">Enquête</a></li>
+            </ul>
+            <p> © Copyright MBO Utrecht.</p>
+        </footer>
+
+</body>
+
+</html>
