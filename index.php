363aede5e8752d30b3322ea9da3a87871e3a6846
ahmedhe_ <ahmedhedni@hotmail.nl>
2022-06-28 11:01:19 +0200

    Homepage tot zover

--- index.php
+++ index.php
@@ -1,1 +1,66 @@
-testest
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!-- Meta tags -->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="all">
+    <meta name="language" content="Dutch">
+    <meta name="author" content="Ahmed al Hedni">
+    <meta name="description" content="">
+    <meta name="keywords" content="OPENDAG, ICT">
+    <meta name="copyright" content="copyright">
+
+    <!-- CSS link -->
+    <link rel="stylesheet" href="style.css">
+
+    <!-- Hamburger link for small screens -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Website title -->
+    <title>MBO Utrecht</title>
+</head>
+
+<body>
+    <div class="topnav">
+        <img src="https://www.mboutrecht.nl/wp-content/themes/mboutrecht/img/output/logo-new@2x.png" class="responsive">
+        <div class="topnav" id="myTopnav">
+            <ul>
+                <a href="./index.php">Home</a>
+                <a href="./overige-info.php">Overige Informatie</a>
+                <a href="./enquete.php">Enquête </a>
+                <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                    <i class="fa fa-bars"></i>
+            </ul>
+            </a>
+        </div>
+    </div>
+    <main>
+        <div id="information">
+            <div class="div">
+                <h1>Wil jij in september 2022 starten met een opleiding, maar heb je nog geen studiekeuze gemaakt?</h1>
+                <h2>Dan ben je bij ons op het juiste adres.</h2>
+                <br>
+                <p><b>Klik gauw op de knop hieronder om meer informatie<br>
+                        over de opendagen te krijgen!</b></p>
+                <br>
+                <button type="button" class="button1"><a href="./overige-info.php"><span></span>MEER INFORMATIE</a></button>
+            </div>
+
+
+        </div>
+
+        <footer class="sticky-footer">
+        <!-- <h2>Footer Stick to the Bottom</h2> -->
+        <ul>
+            <li><a href="./index.php">Home</a></li>
+            <li><a href="./overige-info.php">Overige Informatie</a></li>
+            <li><a href="./enquete.php">Enquête</a></li>
+        </ul>
+        <p> © Copyright MBO Utrecht.</p>
+    </footer>
+
+</body>
+
+</html>
